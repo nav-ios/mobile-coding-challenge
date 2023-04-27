797d318a2d3247db7de1d5a3e15226ebc406f9b1
Nav <navdeep.rana@heyhub.com>
2023-04-27 13:38:42 +0530

    Add `imageloader` `URLSession` implementation

--- PodcastsFeed/Feed Files/PodcastLoaderAPI.swift
+++ PodcastsFeed/Feed Files/PodcastLoaderAPI.swift
@@ -24,7 +24,6 @@ public class PodcastLoaderAPI: PodcastLoader{
                 let arrayPodcasts = Item(json: data).arrayPodcasts
                 completion(.success(arrayPodcasts))
             }
-            
         }
     }
 }
