818c1f33648bd7679e0fdba851fbc2724a713d59
Nav <navdeep.rana@heyhub.com>
2023-04-28 22:51:28 +0530

    Asserts that store `PodcastCache` fails with error on store failing with error

--- PodcastsCacheTests/PodcastsCacheTests.swift
+++ PodcastsCacheTests/PodcastsCacheTests.swift
@@ -26,27 +26,32 @@ class PodcastCache{
     func isFavourite(podcastID: String, completion: @escaping (PodcastCacheResult) -> Void){
         cacheStore.checkForFavourite(podcastID){ result in
             switch result{
-            case true:
-                completion(.success(true))
-            case false:
-                completion(.success(false))
+            case let .success(isSaved):
+                completion(.success(isSaved))
+            case let .failure(error):
+                completion(.failure(error))
             }
         }
     }
 }
 
+enum CacheStoreResult{
+    case success(Bool)
+    case failure(Error)
+}
+
 class CacheStore{
-    var arrayCompletion = [(Bool) -> Void]()
+    var arrayCompletion = [(CacheStoreResult) -> Void]()
     
-    func checkForFavourite(_ id: String, completion: @escaping (Bool) -> Void){
+    func checkForFavourite(_ id: String, completion: @escaping (CacheStoreResult) -> Void){
         arrayCompletion.append(completion)
     }
     
-    func completeWith(error: Error?, message: Bool, at index: Int = 0){
+    func completeWith(error: Error?, message: Bool? = nil, at index: Int = 0){
         if error == nil{
-            arrayCompletion[index](message)
+            arrayCompletion[index](.success(message!))
         }else{
-            arrayCompletion[index](false)
+            arrayCompletion[index](.failure(error!))
         }
     }
     
@@ -68,10 +73,27 @@ final class PodcastCacheTests: XCTestCase {
         }
         store.completeWith(error: nil, message: false)
         wait(for: [exp], timeout: 1.0)
-
         
     }
     
+    
+    func test_save_failsOnStoreFailingWithError(){
+        let (sut, store) = makeSUT()
+        let podcast = makePodcast(title: "iPhone Podcast", description: "Tim Cook delivers WWDC 2023")
+        let exp = expectation(description: "Wait for store")
+        sut.isFavourite(podcastID: podcast.id) { result in
+            switch result{
+            case let .success(_):
+                XCTFail("Expected failure got success")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            }
+            exp.fulfill()
+        }
+        
+        store.completeWith(error: anyError(), message: nil)
+        wait(for: [exp], timeout: 1.0)
+    }
     func makeSUT() -> (PodcastCache, CacheStore){
         let cacheStore = CacheStore()
         let sut = PodcastCache(cacheStore: cacheStore)
@@ -85,6 +107,10 @@ final class PodcastCacheTests: XCTestCase {
            return Podcast(title: title, description: description, id: id, imageURL: anyURL(), thumbnailURL: anyURL(), publisher: "Some publisher")
         }
 
+    private func anyError() -> NSError{
+        NSError(domain: "Any podcast error", code: 10)
+    }
+    
     private func anyURL() -> URL{
         URL(string: "http://any-podcast-url.com")!
     }
