afaa9064d6f93069976fee221f178316b204a50c
nav-ios <appdev1221@gmail.com>
2023-04-29 19:53:41 +0530

    Fixed `import UIKit` issue

--- PodcastsFeed/View Controllers/Second Screen/PodcastDetailsViewComposer.swift
+++ PodcastsFeed/View Controllers/Second Screen/PodcastDetailsViewComposer.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import UIKit
 class PodcastDetailsViewComposer{
     static func composeDetailsViewWith(model: Podcast, imageLoader: ImageLoader){
         let bundle = Bundle(identifier: "com.heyhub.PodcastsFeed")
