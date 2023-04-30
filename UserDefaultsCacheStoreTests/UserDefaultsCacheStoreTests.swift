5e38b973206b24eba3366f562ad345f5332907b5
nav-ios <appdev1221@gmail.com>
2023-04-30 10:08:30 +0530

    Create new test target for `UserDefaultsCacheStore` and set its platform to macOS

---
+++ UserDefaultsCacheStoreTests/UserDefaultsCacheStoreTests.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultsCacheStoreTests.swift
+//  UserDefaultsCacheStoreTests
+//
+//  Created by Nav on 30/04/23.
+//
+
+import XCTest
+
+final class UserDefaultsCacheStoreTests: XCTestCase {
+
+    
+    
+}
