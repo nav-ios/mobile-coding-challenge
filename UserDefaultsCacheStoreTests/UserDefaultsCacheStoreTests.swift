c2a4868a1588463b6ebde321d5f45f2eee08f4ee
nav-ios <appdev1221@gmail.com>
2023-04-30 10:54:16 +0530

    Tests `UserDefaultsCacheStore` results true after a Podcast is favourited by  a user

--- UserDefaultsCacheStoreTests/UserDefaultsCacheStoreTests.swift
+++ UserDefaultsCacheStoreTests/UserDefaultsCacheStoreTests.swift
@@ -6,9 +6,67 @@
 //
 
 import XCTest
+@testable import PodcastsFeed
 
 final class UserDefaultsCacheStoreTests: XCTestCase {
 
+
+    func test_checkForFavourite_resultsFalseWhenPodcastIsNotFavourited(){
+        let sut = UserDefaultsCacheStore()
+        let podcastID = makePodcastID()
+        sut.checkForFavourite(podcastID) { result in
+            switch result{
+            case let .success(isFav):
+                XCTAssertFalse(isFav)
+            case .failure:
+                XCTFail("Expected success with false but got failure")
+            }
+        }
+    }
+    func test_checkForFavourite_returnsTrueAfterPodcastIsFavourited(){
+        let sut = UserDefaultsCacheStore()
+        let podcastID = makePodcastID()
+        sut.checkForFavourite(podcastID) { result in
+            switch result{
+            case let .success(isFav):
+                XCTAssertFalse(isFav)
+            case .failure:
+                XCTFail("Expected success with false but got failure")
+            }
+        }
+        
+        sut.favouriteAction(podcastID) { result in
+            switch result{
+            case let .success(isFav):
+                XCTAssertTrue(isFav)
+            case .failure:
+                XCTFail("Expected success with false but got failure")
+            }
+        }
+        
+        sut.checkForFavourite(podcastID) { result in
+            switch result{
+            case let .success(isFav):
+                XCTAssertTrue(isFav)
+            case .failure:
+                XCTFail("Expected success with false but got failure")
+            }
+        }
+        
+        
+    }
     
     
+    
+    private func makePodcastID() -> String{
+        return UUID().uuidString
+    }
+    
+    override class func tearDown() {
+            let defaults = UserDefaults.standard
+            let dictionary = defaults.dictionaryRepresentation()
+            dictionary.keys.forEach { key in
+                defaults.removeObject(forKey: key)
+            }
+    }
 }
